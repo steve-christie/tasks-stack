ce9ddac36dd7f895590edf0ab89223abb710e49a
steve-christie <steven.christie@hotmail.co.uk>
2023-12-07 13:18:45 +0000

    added readme

---
+++ packages/ui/src/components/landing/LandingPage.tsx
@@ -0,0 +1,9 @@
+import { Page } from "../page/Page";
+import Markdown from "react-markdown";
+export default () => {
+  return (
+    <Page>
+      <Markdown urlTransform={} />
+    </Page>
+  );
+};
